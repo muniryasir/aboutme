efbfa6c9553181333e1eaa64e51b77b12b49fddc
muniryasir <muniryasir2009@gmail.com>
2024-09-01 11:49:09 +0500

    cors

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const userRouter = require('./routes/user');
 require('dotenv').config(); // Load environment variables
 const cors = require('cors');
 
+
+
+const app = express();
+const port = 3000;
 app.use(cors({
     origin: 'http://localhost:3000', // Allow requests from this origin
     methods: ['GET', 'POST'], // Allow these HTTP methods
     credentials: true // Allow cookies to be sent
   }));
-
-const app = express();
-const port = 3000;
-
 app.use(bodyParser.json());
 app.use('/users', userRouter);
 
