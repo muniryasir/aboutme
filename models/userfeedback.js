97ac06bf36d66edc20abcced39956de5efa1aaf1
muniryasir <muniryasir2009@gmail.com>
2024-09-03 12:47:29 +0500

    fix

--- models/userfeedback.js
+++ models/userfeedback.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const userFeedbackSchema = new mongoose.Schema({
     feedbackId: { type: String, unique: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
