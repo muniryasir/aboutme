1f47bdb6eb6f14d9de2af1145dcb6a6a9bc0d19d
muniryasir <muniryasir2009@gmail.com>
2024-09-03 13:16:57 +0500

    cors

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ const cors = require('cors');
 
 const app = express();
 const port = 3000;
-app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
-    methods: ['GET', 'POST'], // Allow these HTTP methods
-    credentials: true // Allow cookies to be sent
-  }));
+// app.use(cors({
+//     origin: 'http://localhost:3000', // Allow requests from this origin
+//     methods: ['GET', 'POST'], // Allow these HTTP methods
+//     credentials: true // Allow cookies to be sent
+//   }));
+app.use(cors())
 app.use(bodyParser.json());
 app.use('/users', userRouter);
 
