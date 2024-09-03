416c07ed10bb4d38c91ed8621efb75677b498357
muniryasir <muniryasir2009@gmail.com>
2024-09-03 18:08:45 +0500

    cors

--- routes/user.js
+++ routes/user.js
@@ -19,6 +19,8 @@ function generateUniqueId(length) {
   
 
 router.post('/generate-id', async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     const { userId } = req.body;
   
     if (!userId) {
